d6b180229fe04cfde34a92c9824326acb7320762
Asadullin Artur <lon.don.91@bk.ru>
2022-09-18 20:27:04 +0200

    make structure project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import {Routes, Route, Link} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Suspense} from "react";
-
 import './styles/index.scss';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames";
+import {Routes, Route, Link} from "react-router-dom";
+import {Suspense} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -17,8 +16,8 @@ const App = () => {
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}></Route>
-                    <Route path={'/'} element={<MainPageAsync/>}></Route>
+                    <Route path={'/about'} element={<AboutPage/>}></Route>
+                    <Route path={'/'} element={<MainPage/>}></Route>
                 </Routes>
             </Suspense>
         </div>
