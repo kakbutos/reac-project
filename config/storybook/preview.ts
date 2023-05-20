ccd4ca97035d5576a67fb551a3f5b3ca371d7129
Asadullin Artur <lon.don.91@bk.ru>
2023-05-20 20:50:32 +0200

    add storybook, add style,theme,router decorator, add story case on component

---
+++ config/storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/react';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { StyleDecorator } from '../../src/shared/config/storybook/RouterDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/StyleDecorator';
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+    },
+    decorators: [
+        StyleDecorator,
+        ThemeDecorator(Theme.LIGHT),
+        RouterDecorator,
+    ],
+};
+
+export default preview;
