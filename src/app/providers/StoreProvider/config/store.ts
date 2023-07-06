8fb8c0162cf290be7db47469f74357f4337a7fef
Asadullin Artur <lon.don.91@bk.ru>
2023-07-06 23:34:27 +0200

    add entity user, login form

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,9 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { StateShema } from './StateShema';
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { userReducer } from 'entities/User';
+import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateShema) {
-    return configureStore<StateShema>({
-        reducer: {},
+export function createReduxStore(initialState?: StateSchema) {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        user: userReducer,
+    };
+
+    return configureStore<StateSchema>({
+        reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider/config/store';
 import { DeepPartial } from '@reduxjs/toolkit';
-import { StateShema } from '../config/StateShema';
+import { StateSchema } from '../config/StateSchema';
 
 interface StoreProviderProps {
     children?: ReactNode;
-    initialState?: DeepPartial<StateShema>;
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
@@ -15,7 +15,7 @@ export const StoreProvider = (props: StoreProviderProps) => {
         initialState,
     } = props;
 
-    const store = createReduxStore(initialState as StateShema);
+    const store = createReduxStore(initialState as StateSchema);
 
     return (
         <Provider store={store}>
