26e945a6562e964da34d3685e7d762f7bff7708d
Asadullin Artur <lon.don.91@bk.ru>
2023-08-14 19:48:25 +0200

    useModal hooks, lazy import lib, provider, gesture and react spring

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,6 +7,7 @@ import { NotificationList } from 'entities/Notification';
 import { Popover } from 'shared/ui/Popups';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
 import { isMobile } from 'shared/lib/device/device';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -35,9 +36,11 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
         return (
             <div>
                 {trigger}
-                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                    <NotificationList />
-                </Drawer>
+                <AnimationProvider>
+                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                        <NotificationList />
+                    </Drawer>
+                </AnimationProvider>
             </div>
         );
     }
