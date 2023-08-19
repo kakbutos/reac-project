5cd2ce7e190a3258f857ca4a3973177ac1fee890
Asadullin Artur <lon.don.91@bk.ru>
2023-08-19 20:33:45 +0200

    fix storybook, update screenshot test

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -2,7 +2,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator';
-import { Theme } from '../../src';
+import { Theme } from '../../src/shared/const/theme';
 
 const preview = {
     parameters: {
@@ -12,6 +12,15 @@ const preview = {
                 color: /(background|color)$/i,
                 date: /Date$/,
             },
+        },
+        layout: 'fullscreen',
+        themes: {
+            default: 'light',
+            list: [
+                { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+                { name: 'dark', class: Theme.DARK, color: '#000000' },
+                { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+            ],
         },
     },
     decorators: [
